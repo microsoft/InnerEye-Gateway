b84e09fd04cf209f5e0c1634d6ac470614e505b4
Jonathan Tripp <v-jontri@microsoft.com>
2021-06-15 16:22:55 +0100

    Add failing string contains test

---
+++ Source/Microsoft.Gateway/Microsoft.InnerEye.DicomConstraints.Tests/StringContainsTests.cs
@@ -0,0 +1,52 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+namespace Microsoft.InnerEye.DicomConstraints.Tests
+{
+    using Dicom;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class StringContainsTests
+    {
+        [TestCategory("StringContainsConstraints")]
+        [TestMethod]
+        public void StringContainsSomeOrdinal()
+        {
+            var dataset = new DicomDataset
+            {
+                { DicomTag.ImageType, new  [] {"ORIGINAL","PRIMARY", "AXIAL" } },
+            };
+
+            for (var i = 0; i < 2; i++)
+            {
+                // The first two components don't contain "XIA"
+                var constraint = new StringContainsConstraint(DicomTag.ImageType, "XIA", i);
+                Assert.IsFalse(constraint.Check(dataset).Result);
+            }
+
+            // The last component does contain "XIA"
+            var constraint2 = new StringContainsConstraint(DicomTag.ImageType, "XIA", 2);
+            Assert.IsTrue(constraint2.Check(dataset).Result);
+
+            // Check that this is case sensitive
+            var constraint3 = new StringContainsConstraint(DicomTag.ImageType, "xia", 2);
+            Assert.IsFalse(constraint3.Check(dataset).Result);
+        }
+
+        [TestCategory("StringContainsConstraints")]
+        [TestMethod]
+        public void StringContainsAnyOrdinal()
+        {
+            var dataset = new DicomDataset
+            {
+                { DicomTag.ImageType, new  [] {"ORIGINAL","PRIMARY", "AXIAL" } },
+            };
+
+            // The last component does contain "XIA"
+            var constraint = new StringContainsConstraint(DicomTag.ImageType, "XIA", -1);
+            Assert.IsTrue(constraint.Check(dataset).Result);
+        }
+
+    }
+}
