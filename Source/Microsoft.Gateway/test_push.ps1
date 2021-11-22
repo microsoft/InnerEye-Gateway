b20f7cdd00468d9db79ae6f011d0cec4a797df18
Jonathan Tripp <v-jontri@microsoft.com>
2021-11-22 10:29:16 +0000

    Send H&N model

--- Source/Microsoft.Gateway/test_push.ps1
+++ Source/Microsoft.Gateway/test_push.ps1
@@ -3,9 +3,9 @@
 	--scan-directories                     <# scan directories for input files #> `
 	--scan-pattern "*.dcm"                 <# pattern for filename matching (wildcards) #> `
 	--recurse                              <# recurse within specified directories #> `
-	--aetitle RADIOMICS_APP                       <# set my calling AE title #> `
-	--call PassThroughModel                      <# set called AE title of peer #> `
+	--aetitle RADIOMICS_APP                <# set my calling AE title #> `
+	--call PassThroughModel                <# set called AE title of peer #> `
 	--config-file ".\Microsoft.InnerEye.Listener.Tests.Common\Assets\SCU.cfg" LEExplicitCT <# use profile LEExplicitiCT from config file #> `
 	127.0.0.1                              <# peer #> `
 	111                                    <# port #> `
-	"..\..\Images\LargeSeriesWithContour\" <# dcmfile-in #>
+	"..\..\Images\HN\"                     <# dcmfile-in #>
